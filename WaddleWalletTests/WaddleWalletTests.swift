d42ec1e9f82e3242adff65a076e76b540229f86e
Matt McCoy <mattcmccoy01@gmail.com>
2025-04-12 08:21:10 -0400

    Initial Commit

---
+++ WaddleWalletTests/WaddleWalletTests.swift
@@ -0,0 +1,16 @@
+//
+//  WaddleWalletTests.swift
+//  WaddleWalletTests
+//
+//  Created by Matt McCoy on 4/12/25.
+//
+
+import Testing
+
+struct WaddleWalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
